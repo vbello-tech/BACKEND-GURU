81dc01e207832dabff6f082246c925da92915507
Vbello-Tech <69384115+vbello-tech@users.noreply.github.com>
2023-05-29 08:16:45 +0100

    runtime

--- manage.py
+++ manage.py
@@ -2,11 +2,15 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import dotenv
+from decouple import config
 
 
 def main():
     """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'Backend.settings.development')
+    if os.getenv('DJANGO_SETTINGS_MODULE'):
+        os.environ['DJANGO_SETTINGS_MODULE'] = os.getenv('DJANGO_SETTINGS_MODULE')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
