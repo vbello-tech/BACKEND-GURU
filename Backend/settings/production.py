1fee4660938d03a44fc4b0edaa62993d7fde5b92
Vbello-Tech <69384115+vbello-tech@users.noreply.github.com>
2023-05-29 22:42:27 +0100

    DB

--- Backend/settings/production.py
+++ Backend/settings/production.py
@@ -1,11 +1,18 @@
 from .base import *
+import dj_database_url
 
 # Database
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
-DATABASES = {
+"""DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
     }
+}"""
+
+DATABASES = {
+    'default':  dj_database_url.config(
+        default=config('DATABASE_URL')
+    )
 }
